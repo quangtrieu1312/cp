8d30c27f2e5a0ca8301476d3bd718bcb08d3b602
trieutruongopswat <trieu.truong@opswat.com>
2022-06-22 12:43:25 +0700

    1700D: getting started

---
+++ codeforces/1700D/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, q, res;
+vector<int> v, t;
+int main()
+{
+    cin>>n;
+    v.resize(n);
+    for (int i=0; i<n; i++){
+        cin>>v[i];
+    }
+    cin>>q;
+    t.resize(q);
+    for (int i=0; i<q; i++){
+        cin>>t[i];
+    }
+
+    res=0;
+    cout<<res;
+    return 0;
+}
