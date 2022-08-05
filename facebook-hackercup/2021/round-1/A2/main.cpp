7dce2411c8b44c82113a67e2590c989d93157a92
trieutruongopswat <trieu.truong@opswat.com>
2022-08-05 13:29:37 +0700

    Facebook Hackercup pass round 1

--- facebook-hackercup/2021/round-1/A2/main.cpp
+++ facebook-hackercup/2021/round-1/A2/main.cpp
@@ -1,40 +1,59 @@
 #include <bits/stdc++.h>
 #define modulo 1000000007
 using namespace std;
+typedef long long ll;
 int T, N;
 string W;
-vector<long long> dp;
+vector<ll> dp, cnt_v, cnt_tail_f_v;
 int main()
 {
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     cin>>T;
     for (int casenum=0; casenum<T; casenum++){
-        dp.resize(0);
         cin>>N;
         cin>>W;
-        string tmp="";
-        for (int i=0; i<N; i++){
-            if (W[i]!='F'){
-                tmp+=W[i];
+        cnt_v.resize(0);
+        cnt_tail_f_v.resize(0);
+        int cnt=1, cnt_tail_f=(W[0]=='F');
+        char c=(W[0]=='F')?'.':W[0];
+        for (int i=1; i<N; i++){
+            if (c=='.'){
+                if (W[i]!='F'){
+                    c=W[i];
+                    cnt_tail_f=0;
+                } else {
+                    cnt_tail_f++;
+                }
+                cnt++;
+            } else {
+                if (W[i]!='F' && W[i]!=c){
+                    cnt_v.push_back(cnt);
+                    cnt_tail_f_v.push_back(cnt_tail_f);
+                    c=W[i];
+                    cnt=1;
+                    cnt_tail_f=0;
+                } else if (W[i]!='F') {
+                    cnt++;
+                    cnt_tail_f=0;
+                } else {
+                    cnt++;
+                    cnt_tail_f++;
+                }
             }
         }
-        W=tmp;
-        N=W.length();
+        cnt_v.push_back(cnt);
+        cnt_tail_f_v.push_back(cnt_tail_f);
+        N=cnt_v.size();
+        dp.resize(0);
         dp.resize(N,0);
-        char c=W[0];
-        int cnt=1, dd;
+        ll cur_sum_cnt=cnt_v[0], dd=0, debugres, cur_sum_tail_f=cnt_tail_f_v[0];
         for (int i=1; i<N; i++){
-            dd=0;
-            if (W[i]!=c){
-                dp[i]=(dp[i-1]+cnt)%modulo;
-                c=W[i];
-                cnt=1;
-            } else {
-                cnt++;
-                dp[i]=(dp[i-1]+dp[i-cnt+1])%modulo;
-            }
-            dd=dp[i];
+            dd=(dd+cur_sum_cnt)%modulo;
+            dp[i]=(dp[i-1]+(dd-cur_sum_tail_f)*cnt_v[i])%modulo;
+            cur_sum_cnt=cur_sum_cnt+cnt_v[i];
+            cur_sum_tail_f=cur_sum_tail_f+cnt_tail_f_v[i];
+            debugres=dp[i];
         }
         cout<<"Case #"<<casenum+1<<": "<<dp[N-1]<<endl;
     }
