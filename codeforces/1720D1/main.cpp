544c42d8b2efb64b906d379f00cc00e0dd3c011d
Trieu Truong <quangtrieu1312@gmail.com>
2022-09-06 00:20:14 +0700

    Facebook Hackercup 2022 qualification round

---
+++ codeforces/1720D1/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int t, n, res;
+vector<int> a, dp;
+int main()
+{
+    cin>>t;
+    while (t--){
+        cin>>n;
+        a.resize(n);
+        for (int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        dp.resize(0);
+        dp.resize(n,0);
+        dp[0]=1;
+        for (int i=1; i<n; i++){
+            dp[i]=1;
+            for (int j=max(0,i-400); j<i; j++){
+                if ((a[j]^i)<(a[i]^j)){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        res=0;
+        for (int i=0; i<n; i++){
+            res=max(res,dp[i]);
+        }
+        cout<<res<<endl;
+    }
+    return 0;
+}
